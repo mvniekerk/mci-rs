302fd2464e24ce0a83aed913e173b6b01c6080d7
Michael van Niekerk <mvniekerk@chazaqdev.com>
2020-07-30 12:57:45 +0200

    CCCR blocksize LSB

--- src/sd_mmc/sdio/registers/cccr/block_size.rs
+++ src/sd_mmc/sdio/registers/cccr/block_size.rs
@@ -1,11 +1,31 @@
 use crate::sd_mmc::sdio::registers::registers::Register;
+use bit_field::BitField;
 
+/// FN0 block size, LSB first
 pub struct BlockSize {
-    pub val: u8
+    pub val: u16
 }
 
-impl Register<u8> for BlockSize {
-    fn value(&self) -> u8 {
+impl BlockSize {
+    pub fn set_lsb(&mut self, val: u8) {
+        self.val.set_bits(8..15, val as u16);
+    }
+
+    pub fn lsb(&self) -> u8 {
+        self.val.get_bits(8..15) as u8
+    }
+
+    pub fn set_msb(&mut self, val: u8) {
+        self.val.set_bits(0..8, val as u16);
+    }
+
+    pub fn msb(&mut self) -> u8 {
+        self.val.get_bits(0..8) as u8
+    }
+}
+
+impl Register<u16> for BlockSize {
+    fn value(&self) -> u16 {
         self.val
     }
 
