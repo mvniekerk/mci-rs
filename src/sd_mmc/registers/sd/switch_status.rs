246810cc44737bdf4407bd9d40b1d6cc37ad1144
Michael van Niekerk <mvniekerk@chazaqdev.com>
2020-08-03 12:09:23 +0200

    Get bits in 512 register

--- src/sd_mmc/registers/sd/switch_status.rs
+++ src/sd_mmc/registers/sd/switch_status.rs
@@ -16,7 +16,6 @@ impl Register<[u64; 8]> for SwitchStatusRegister {
 }
 
 impl SwitchStatusRegister {
-    // TODO test this
     pub fn set_bits(&mut self, val: u16, offset: u16, len: usize) {
         let index = offset / 64;
         let p_offset = (offset - (index * 64)) as usize;
@@ -41,6 +40,23 @@ impl SwitchStatusRegister {
         }
     }
 
+    /// Largest get is 16 bits, hence u16 as return
+    pub fn get_bits(&self, offset: u16, len: usize) -> u16 {
+        let index = offset / 64;
+        let p_offset = (offset - (index * 64)) as usize;
+        let index = index as usize;
+        let mut current = self.val[index];
+        if p_offset < 64 - len {
+            let range = p_offset..(p_offset + len);
+            current.get_bits(range) as u16
+        }  else {
+            let shift = 64 - p_offset;
+            let mut val = current.get_bits(p_offset..64) as u16;
+            current = self.val[index];
+            val | (current.get_bits(0..shift) as u16)
+        }
+    }
+
 
 }
 
@@ -49,10 +65,8 @@ mod tests {
     use crate::sd_mmc::registers::sd::switch_status::SwitchStatusRegister;
 
     #[test]
-    fn test_stuffing_16_bits() {
-        let mut reg = SwitchStatusRegister {
-            val: [0u64; 8]
-        };
+    fn test_setting_bits() {
+        let mut reg = SwitchStatusRegister { val: [0u64; 8] };
 
         reg.set_bits(0b10101, 128, 5);
         assert_eq!(reg.val[0], 0);
@@ -65,6 +79,21 @@ mod tests {
         assert_eq!(reg.val[0], 0);
         assert_eq!(reg.val[1], 0xC000_0000_0000_0000);
         assert_eq!(reg.val[2], 0b111);
+    }
+
+    #[test]
+    fn test_getting_bits() {
+        let mut reg = SwitchStatusRegister { val: [0u64; 8] };
+
+        reg.set_bits(0xa0a0, 80, 16);
+        reg.set_bits(0x5555, 96, 16);
+
+        let bits = reg.get_bits(88, 16);
+        assert_eq!(bits, 0x55a0);
+        let bits = reg.get_bits(92, 8);
+        assert_eq!(bits, 0x5a);
+        let bits = reg.get_bits(94, 4);
+        assert_eq!(bits, 0x06);
     }
 
 }
